159b6b6f21b0d61d30bb4373ddc42588990f6357
Indra Kumar Prajapati <9919indrakumar@gmail.com>
2026-04-05 23:02:50 +0530

    Add files via upload

---
+++ Day2.py
@@ -0,0 +1,14 @@
+#Write a Python program to do arithmetical operations addition and division.
+num1=int(input("Enter 1st No."))
+num2=int(input("Enter 2nd No."))
+sum=num1+num2
+print(sum)
+if num2==0:
+    print("Zero division not possible")
+else:
+    division=num1/num2
+    print(division)
+
+
+
+
