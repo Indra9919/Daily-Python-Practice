5e9fed8d5a8af149376915605ec3e952877e914d
Indra Kumar Prajapati <9919indrakumar@gmail.com>
2026-04-06 23:13:21 +0530

    Add files via upload

---
+++ Day3.py
@@ -0,0 +1,5 @@
+'''Write a Python program to find the area of a triangle.'''
+base=float(input("Enter base of triangle "))
+height=float(input("Enter height of triangle "))
+Area=0.5*base*height
+print(Area)
